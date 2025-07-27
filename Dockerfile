79461c104252e7d5e403ce057b8ceaf418c963d0
Monya <isadom@mail.ru>
2025-07-27 14:28:49 +0400

    Добавил кнопку с доступом к базе данных

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,7 @@
 FROM openjdk:21
-#COPY target/demoSpring-0.0.1-SNAPSHOT.jar /usr/src/my-app/demoSpring-0.0.1-SNAPSHOT.jar
+#Править название
+COPY target/demoSpring-0.0.1-SNAPSHOT.jar /usr/src/my-app/demoSpring-0.0.1-SNAPSHOT.jar
 WORKDIR /usr/src/my-app
 EXPOSE 8090
-#CMD java -jar demoSpring-0.0.1-SNAPSHOT.jar
+#Править название
+CMD java -jar demoSpring-0.0.1-SNAPSHOT.jar
--- src/main/java/ru/monya/firstWebSB/controllers/HomeController.java
+++ src/main/java/ru/monya/firstWebSB/controllers/HomeController.java
@@ -10,12 +10,12 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class HomeController {
     @RequestMapping(value = "/home")
     public String home() {
-        return "redirect:index3.html";
+        return "redirect://homePage.html";
     }
 
     @RequestMapping("/other-page")
     public String otherPages1() {
-        return "redirect:index2.html";
+        return "redirect://index2.html";
     }
 
 
@@ -24,3 +24,5 @@ public class HomeController {
 }
 //Это пиздец товарищи, - простая команда  redirect: перенаправляет на статические ресурсы при использовании шаблонов Thymeleaf.
 // Два дня ушло, чтобы найти эту инфу,- в англоязычном интернете. А какого только говна не предлагали, - даже отдельный класс конфигурации создать
+
+//Еще фишка без двух косых после  redirect: страница не обновляется в браузере с в старой вкладе, - чтобы увидеть обновленную страницу нужно ввести адрес во вновь созданной вкладке
